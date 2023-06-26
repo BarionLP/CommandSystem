7f26239b2b02e9d3805d1d1ba0f28afb98b617ae
Barion <kothe@botos.de>
2023-06-26 18:07:57 +0200

    init

---
+++ Runtime/ConsoleCommandHandler.cs
@@ -0,0 +1,17 @@
+using System;
+using Ametrin.Console;
+
+namespace Ametrin.Command.Console{
+    #nullable enable
+    public sealed class ConsoleCommandHandler : IConsoleHandler {
+        public bool PassPrefix => false;
+
+        public void Execute(string input)=> CommandManager.Execute(input);
+
+        public string? GetSyntax(string value){
+            var inputParts = value.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if(inputParts.Length == 0) return null;
+            return CommandManager.GetSyntax(inputParts[0]);
+        }
+    }
+}
---
+++ Runtime/IComandArgumentParser.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Ametrin.Command{
+    public interface ICommandArgumentParser{
+        public object Parse(string raw);
+
+        public string[] GetSuggestions() => Array.Empty<string>();
+    }
+}
